ed0807177b07e0ac18fd2c1426e4719f5cab25a7
pB <pavel.wh@yandex.ru>
2019-12-28 18:24:12 +0300

    Shorten links backend module

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = (req, res, next) => {
+    if(req.method = 'OPTIONS') {
+        return next()
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1] // Bearer TOKEN
+
+        if(!token) {
+            res.status(401).json({ message: 'Пользователь не авторизован'})
+        }
+
+        const decoded = jwt.verify(token, config.get('secretJWT'))
+
+        req.user = decoded
+        next()
+    } catch (error) {
+        res.status(401).json({ message: 'Пользователь не авторизован'})
+    }
+}
