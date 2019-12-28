e7a9aaa7e78fbea18c54362b3109244a409d1f62
pB <pavel.wh@yandex.ru>
2019-12-28 17:02:01 +0300

    Navbar in authorized pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useRoutes } from './routes'
 import { useAuth } from './hooks/auth.hook'
 import 'materialize-css'
 import { AuthContext } from './context/AuthContext'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const { token, login, logout, userId } = useAuth()
@@ -14,6 +15,7 @@ function App() {
       token, login, logout, userId, isAuthenticated
     }}>
       <Router>
+        { isAuthenticated && <Navbar /> }
         <div className='container'>
           { routes }
         </div>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export const Navbar = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const logoutHandler = event => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return (
+        <nav>
+            <div className="nav-wrapper yellow darken-4" style={{ padding: '0 1.5rem' }}>
+                <span className="brand-logo">Сокращай</span>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/create">Создать</NavLink></li>
+                    <li><NavLink to="/links">Ссылки</NavLink></li>
+                    <li><a href="/" onClick={ logoutHandler }>Выйти</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
