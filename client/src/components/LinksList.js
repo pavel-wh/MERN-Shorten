b0d9086d0d30296725ca803749f595c425212e56
pB <webforeman@gmail.com>
2020-06-28 01:35:26 +0300

    Created adaptive table

--- client/src/components/LinksList.js
+++ client/src/components/LinksList.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 
 export const LinksList = ({ links }) => {
-    if(!links.length) {
-        return <p className="center">Ссылок пока нет...</p>
-    }
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>№</th>
-                    <th>Оригинальная</th>
-                    <th>Сокращённая</th>
-                    <th>Количество кликов</th>
-                    <th>Открыть</th>
-                </tr>
-            </thead>
-            <tbody>
-                { links.map((link, index) => {
-                    return (
-                        <tr key={ link._id }>
-                            <td>{ index + 1}</td>
-                            <td>{ link.from }</td>
-                            <td>{ link.to }</td>
-                            <td>{ link.clicks }</td>
-                            <td>
-                                <Link to={`/detail/${ link._id }`}>Открыть</Link>
-                            </td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-    )
+  if (!links.length) {
+    return <p className="center">Ссылок пока нет...</p>
+  }
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>№</Th>
+          <Th>Оригинальная</Th>
+          <Th>Сокращённая</Th>
+          <Th>Количество кликов</Th>
+          <Th>Открыть</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {links.map((link, index) => {
+          return (
+            <Tr key={link._id}>
+              <Td>{index + 1}</Td>
+              <Td>{link.from}</Td>
+              <Td>{link.to}</Td>
+              <Td>{link.clicks}</Td>
+              <Td>
+                <Link to={`/detail/${link._id}`}>Открыть</Link>
+              </Td>
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </Table>
+  )
 }
